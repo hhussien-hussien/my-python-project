1589004a089490bb549b3d8c173ba3c0b48ba913
Hussien Hussien <hhussien@umd.edu>
2025-08-31 12:22:43 -0400

    Initial commit: added main.py and greet.py

---
+++ Untitled-2.py
@@ -0,0 +1,42 @@
+# libraries to be used
+import matplotlib.pyplot as plt
+import math
+
+
+
+# functions to be used
+from greet import greet   # import the greet function
+from math_functions import cal   # import the cal function
+
+# print("Hello, Python in VS Code!")
+a = 4 
+b = a * 2
+print(b) 
+print(a > b)   # True
+
+greet()
+
+i, return1, return2 = cal(a,b)
+
+# return2 = subtract(a,b)
+
+print(return1)
+
+print(return2)
+
+print(i)
+
+# Example data
+x = [0, 1, 2, 3, 4]
+y = [0, 1, 4, 9, 16]
+
+# Plot
+plt.plot(x, y)
+
+# Add title and labels
+plt.title("Simple Line Plot")
+plt.xlabel("X-axis")
+plt.ylabel("Y-axis")
+
+# Show the plot
+plt.show()
---
+++ math_functions.py
@@ -0,0 +1,27 @@
+# math_functions.py
+import math
+
+def cal(a, b):
+    m = a + b
+    d = math.sin(a - b) 
+
+
+    i = 1
+    
+    k = 0 
+    #for i in range(5):  
+        # print(i)
+    
+
+    #if (i<5):
+    #    print("YES!")
+
+    while (i<5) :
+        i = i + 1 
+
+    print(i)
+
+    return i, m, d   # returns the sum
+
+# def subtract(a, b):
+#    return a - b   # returns the difference
